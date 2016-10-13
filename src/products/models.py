ddda54e5dada0ff5fbb8b3dd2b1be11c0903e043
Sam <samson.antwi123@gmail.com>
2016-10-13 21:31:02 +0100

    unique slugs created

--- src/products/models.py
+++ src/products/models.py
@@ -1,11 +1,17 @@
+from django.conf import settings
+
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
+from django.utils.text import slugify
+
 # Create your models here.
 
 
 class Product(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL)
+	managers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="managers_product", blank=True)
 	title = models.CharField(max_length=30) #;lasfdjksdl;aj;lasdjsdaj
-	slug = models.SlugField(default='slug-field') #unique=True
+	slug = models.SlugField(blank=True, unique=True) 
 	description = models.TextField()
 	price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99)  #100.00
 	sale_price = models.DecimalField(max_digits=100, decimal_places=2, default=6.99, null=True, blank=True)  #100.00
@@ -14,6 +20,32 @@ class Product(models.Model):
 		return self.title
 
 
-	def product_pre_save_reciever(sender, instance, *args, **kwargs):
-		print(sender)
-		print(instance)
+def create_slug(instance, new_slug=None):
+	slug = slugify(instance.title)
+	if new_slug is not None:
+		slug = new_slug
+
+	qs = Product.objects.filter(slug=slug)
+	exists = qs.exists()
+	if qs:
+		new_slug = "%s-%s" % (slug, qs.first().id)
+		return create_slug(instance, new_slug=new_slug)
+	return slug
+
+
+def product_pre_save_reciever(sender, instance, *args, **kwargs): #with args and kwargs any additional keyword arguments wont cause an error
+	"""makes sure the product is slugifyed before the instance is saved on the database """
+	if not instance.slug:
+		instance.slug = create_slug(instance)
+
+pre_save.connect(product_pre_save_reciever,sender=Product)
+
+
+# def product_post_save_reciever(sender, instance, *args, **kwargs): #with args and kwargs any additional keyword arguments wont cause an error
+""""""
+
+# 	if instance.slug != slugify(instance.title):
+# 		instance.slug = slugify(instance.title)
+# 		instance.save()
+
+# pre_save.connect(product_pre_save_reciever,sender=Product)
--- src/products/views.py
+++ src/products/views.py
@@ -1,13 +1,57 @@
+
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+
 from .models import Product
 from .forms import ProductAddForm, ProductModelForm
+from digitalmarket.mixins import MultiSlugMixin, SubmitBtnMixin
+
+
+class ProductCreateView(SubmitBtnMixin, CreateView):
+
+	model = Product
+	template_name = "form.html"
+	form_class = ProductModelForm
+	success_url = "/products/add"
+	submit_btn = "Add Product"
+
+
+class ProductUpdateView(SubmitBtnMixin, MultiSlugMixin, UpdateView):
+	model = Product
+	template_name = "form.html"
+	form_class = ProductModelForm
+	success_url = "/products/"
+	submit_btn = "Update Product"
+
+	def get_object(self, *args, **kwargs):
+		"""it looks to see if user in managers and check if user is authenticated"""
+		user = self.request.user
+		obj = super(ProductUpdateView, self).get_object(*args, **kwargs) #superclass of current view
+		if obj.user == user or user in obj.managers.all():
+			return obj
+		else:
+			raise Http404
+
+
+class ProductDetailView(MultiSlugMixin, DetailView):
+	model = Product
+
+
+class ProductListView(ListView):
+	model = Product
+
+	def get_queryset(self, *args, **kwargs):
+		qs = super(ProductListView, self).get_queryset(**kwargs)
+		# qs = qs.filter(title__icontains="aaaaaaa")
+		# print(qs)
+		return qs
 
 
 # Create your views here.
 def create_view(request):
-	#FORM
-	# print(request.POST)
 	form = ProductModelForm(request.POST or None) #create instance by adding ()
 	if form.is_valid():
 		instance = form.save(commit=False)
@@ -15,13 +59,30 @@ def create_view(request):
 		instance.save()
 		# form.save()
 
-	template = "create_view.html"
+	template = "form.html"
 	context = {
 		"form":form,
+		"submit_btn":"submit button",
 
 	}
 	return render(request, template, context)
 
+
+def update_view(request, object_id=None):
+	"""the queryset below will check object has an id , if not raise 404 error"""
+	product = get_object_or_404(Product, id=object_id)
+	form = ProductModelForm(request.POST or None, instance=product)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+
+	template = "form.html"
+	context = {
+		"object": product,
+		"form": form,
+		"submit_btn": "Update Product",
+	}
+	return render(request, template, context)
 
 
 
@@ -58,3 +119,6 @@ def list_view(request):
 		"queryset": queryset,
 	}
 	return render(request, template, context)
+
+
+
